c65c0d6c1c2a4dba0443236f4e0b9f4f07db59d9
choisumin <tnals634@naver.com>
2023-07-07 14:20:09 +0900

    feat: 게시글 작성 구현
    게시글 작성 구현

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3010;
 
-const userRouter = require("./routes/users.route");
+const userRouter = require('./routes/users.route');
+const postRouter = require('./routes/posts.route');
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/", [userRouter]);
+app.use('/', [userRouter, postRouter]);
 
 app.listen(port, () => {
-  console.log(port, "포트로 서버가 열렸습니다.");
+  console.log(port, '포트로 서버가 열렸습니다.');
 });
---
+++ routes/posts.route.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const { Op } = require('sequelize');
+const authMiddleWare = require('../middlewares/auth-middleware');
+
+const { posts, users } = require('../models');
+
+//게시글 작성 API
+router.post('/posts', authMiddleWare, async (req, res) => {
+  const { title, content } = req.body;
+  const { user_id } = res.locals.user;
+
+  const user = await users.findOne({ where: { user_id } });
+  try {
+    if (!title || !content) {
+      return res
+        .status(400)
+        .json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
+    } else if (typeof title != 'string') {
+      return res
+        .status(400)
+        .json({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
+    } else if (typeof content != 'string') {
+      return res
+        .status(400)
+        .json({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
+    }
+
+    await posts.create({
+      User_id: user_id,
+      nickname: user.nickname,
+      title,
+      content,
+    });
+
+    return res.status(201).json({ message: '게시글 작성에 성공하였습니다.' });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ errorMessage: '게시글 작성에 실패하였습니다.' });
+  }
+});
+
+module.exports = router;
