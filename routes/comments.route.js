f672d95cd09fc19a4dfdb9e6a6aca35bd60e6cc2
choisumin <tnals634@naver.com>
2023-07-07 15:48:02 +0900

    feat: 댓글 수정 구현

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -72,4 +72,58 @@ router.get('/posts/:post_id/comments', async (req, res) => {
   }
 });
 
+//게시글 수정 API
+router.put(
+  '/posts/:post_id/comments/:comment_id',
+  authMiddleWare,
+  async (req, res) => {
+    const { post_id, comment_id } = req.params;
+    const { comment } = req.body;
+    const { user_id } = res.locals.user;
+
+    try {
+      const post = await posts.findOne({ where: { post_id } });
+      const findComment = await comments.findOne({ where: { comment_id } });
+      if (!post) {
+        return res
+          .status(404)
+          .json({ errorMessage: '게시글이 존재하지 않습니다.' });
+      } else if (!findComment) {
+        return res
+          .status(404)
+          .json({ errorMessage: '댓글이 존재하지 않습니다.' });
+      } else if (!comment) {
+        return res.status(400).json({ errorMessage: '댓글을 입력해주세요.' });
+      } else if (typeof comment != 'string') {
+        return res
+          .status(400)
+          .json({ errorMessage: '댓글의 형식이 일치하지 않습니다.' });
+      } else if (user_id != findComment.User_id) {
+        return res
+          .status(403)
+          .json({ errorMessage: '댓글의 수정 권한이 존재하지 않습니다.' });
+      }
+
+      await comments.update(
+        { comment },
+        {
+          where: {
+            [Op.and]: [
+              { comment_id },
+              { Post_id: post_id },
+              { User_id: user_id },
+            ],
+          },
+        }
+      );
+
+      res.json({ message: '댓글을 수정하였습니다.' });
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ errorMessage: '댓글 수정에 실패하였습니다.' });
+    }
+  }
+);
+
 module.exports = router;
